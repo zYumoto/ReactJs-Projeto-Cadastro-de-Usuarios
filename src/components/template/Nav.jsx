a78654f7c581d8a2363b27af02e19fab5e8f2bf4
Victor Juan <victorjuanrazer@gmail.com>
2022-11-18 05:33:28 -0300

    ADD>Nav

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -2,5 +2,15 @@ import "./Nav.css";
 import React from "react";
 
 export default (props) => {
-  return <aside className="menu-area">Navegação</aside>;
-};
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        <a href="#/">
+          <i className="fa fa-home"></i> Inicio
+        </a>
+        <a href="#/users">
+          <i className="fa fa-users"></i> Usuarios
+        </a>
+      </nav>
+    </aside>
+  );};
